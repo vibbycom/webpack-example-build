772cd19ae555c23ed0a7dc2da7a436157a3b544b
Luka Vidakovic <luka.vidakovic@gmail.com>
2017-10-31 14:44:28 +0100

    another class example

--- src/views/home.js
+++ src/views/home.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import styles from '../style/main'
 
-const Home = () => <h1 className={styles.bold}>Home page</h1>
+const Home = () => (
+  <div>
+    <h1 className={styles.bold}>Home page</h1>
+    <p className={styles.text}>Text test, another class, Text test, another class, Text test, another class, Text test, another class</p>
+  </div>
+)
 
 export default Home
